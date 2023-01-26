81f0a7bebfaa028a9ca9c58d0c09122fba014f71
Anna Petelin <andorjamb@protonmail.com>
2023-01-26 15:55:50 +0200

    add form to contact component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
 
 const Header = () => {
     return (
         <div>
-            
+            <Breadcrumb>
+            </Breadcrumb>
+
         </div>
     );
 };
