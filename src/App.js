35ed0a5f7a1c7cfc5085b916eb7278401d25c404
Anna Petelin <andorjamb@protonmail.com>
2023-01-26 10:42:25 +0200

    add BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Layout from './pages/Layout';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
+        <Routes >
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />}></Route>
+            <Route></Route>
 
-    </div>
+
+          </Route>
+        </Routes>
+      </BrowserRouter> </div >
   );
 }
 
